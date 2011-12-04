80929d7e5dbd87e55ed21dd2ea9c57b0e42ade14
nseo <sonots@gmail.com>
2011-12-04 01:07:14 -0800

    support header and image only

--- convert.rb
+++ convert.rb
@@ -11,31 +11,32 @@ pages = db.execute('select name, text from wiki w2 where version = (select max(v
 pages.each do |title, body|
   File.open(File.join(OUT_PATH, title.gsub(/\s/, '') + '.rst'), 'w') do |file|
     body.gsub!(/\r/, '')
+    # pre-formatted texts (not yet)
     body.gsub!(/\{\{\{([^\n]+?)\}\}\}/, '@\1@')
     body.gsub!(/\{\{\{\n#!([^\n]+?)(.+?)\}\}\}/m, '<pre><code class="\1">\2</code></pre>')
     body.gsub!(/\{\{\{(.+?)\}\}\}/m, '<pre>\1</pre>')
-# macro
-    body.gsub!(/\[\[BR\]\]/, '')
-    body.gsub!(/\[\[PageOutline.*\]\]/, '{{toc}}')
-    body.gsub!(/\[\[Image\((.+?)\)\]\]/, '!\1!')
-# header
-    body.gsub!(/=====\s(.+?)\s=====/, "h5. #{'\1'} \n\n")
-    body.gsub!(/====\s(.+?)\s====/,   "h4. #{'\1'} \n\n")
-    body.gsub!(/===\s(.+?)\s===/,     "h3. #{'\1'} \n\n")
-    body.gsub!(/==\s(.+?)\s==/,       "h2. #{'\1'} \n\n")
-    body.gsub!(/=\s(.+?)\s=[\s\n]*/,  "h1. #{'\1'} \n\n")
-# table
+    # macro
+    body.gsub!(/\[\[BR\]\]/, '') # not supported yet
+    body.gsub!(/\[\[PageOutline.*\]\]/, '') # not supported yet
+    body.gsub!(/\[\[Image\((.+?)\)\]\]/, '.. image:: \1')
+    # header (modify to ensure that the line texts are always longer than header texts.)
+    body.gsub!(/=====\s(.+?)\s=====/, "#{'\1'}\n.....................................................\n\n")
+    body.gsub!(/====\s(.+?)\s====/,   "#{'\1'}\n:::::::::::::::::::::::::::::::::::::::::::::::::::::\n\n")
+    body.gsub!(/===\s(.+?)\s===/,     "#{'\1'}\n`````````````````````````````````````````````````````\n\n")
+    body.gsub!(/==\s(.+?)\s==/,       "#{'\1'}\n-----------------------------------------------------\n\n")
+    body.gsub!(/=\s(.+?)\s=[\s\n]*/,  "#{'\1'}\n=====================================================\n\n")
+    # table (not yet)
     body.gsub!(/\|\|/,  "|")
-# link
+    # link (not yet)
     body.gsub!(/\[(http[^\s\[\]]+)\s([^\[\]]+)\]/, ' "\2":\1' )
     body.gsub!(/\[([^\s]+)\s(.+)\]/, ' [[\1 | \2]] ')
     body.gsub!(/([^"\/\!])(([A-Z][a-z0-9]+){2,})/, ' \1[[\2]] ')
     body.gsub!(/\!(([A-Z][a-z0-9]+){2,})/, '\1')
-# text decoration
-    body.gsub!(/'''(.+)'''/, '*\1*')
-    body.gsub!(/''(.+)''/, '_\1_')
-    body.gsub!(/`(.+)`/, '@\1@')
-# itemize
+    # text decoration
+    body.gsub!(/'''(.+)'''/, '**\1**') # bold
+    body.gsub!(/''(.+)''/, '*\1*') # italic
+    body.gsub!(/`(.+)`/, '``\1``') # proportional
+    # itemize (not yet)
     body.gsub!(/^\s\s\s\*/, '***')
     body.gsub!(/^\s\s\*/, '**')
     body.gsub!(/^\s\*/, '*')
